70c2dc4e1c90abc2678102bd00b542fe2fe5f995
EduardoL0pes <113311412+EduardoL0pes@users.noreply.github.com>
2024-09-13 19:55:19 -0300

    aula04 modulos

---
+++ Aula04/modulos.py
@@ -0,0 +1,11 @@
+#import math
+#num = int(input('Digite um número: '))
+#raiz = math.sqrt(num)
+#print('A raiz do número {} é {}'.format(num, math.ceil(raiz)))
+# para importar toda biblioteca math
+
+from math import sqrt, ceil
+num = int(input('Digite um número: '))
+raiz = sqrt(num)
+print('A raiz do número {} é {}'.format(num, ceil(raiz)))
+# para importar somente os itens específicos ex: sqrt e ceil
