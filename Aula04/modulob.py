229038a4df6993403faad8cc7f1a8e6cf8a1b8f0
EduardoL0pes <113311412+EduardoL0pes@users.noreply.github.com>
2024-09-14 14:44:50 -0300

    aula04 modulos - utilizando emojis

---
+++ Aula04/modulob.py
@@ -0,0 +1,9 @@
+#import random
+#num = random.random()
+#print(num)
+#gera um número aleatório
+
+import random
+num = random.randint(1, 10)
+print(num)
+#gera um número aleatório inteiro entre 1 á 10
--- Aula04/modulos.py
+++ Aula04/modulos.py
@@ -8,4 +8,4 @@ from math import sqrt, ceil
 num = int(input('Digite um número: '))
 raiz = sqrt(num)
 print('A raiz do número {} é {}'.format(num, ceil(raiz)))
-# para importar somente os itens específicos ex: sqrt e ceil
+# para importar somente os itens específicos de math ex: sqrt e ceil
