3de3a20c40abeb1d4c807803e8b47dbcf0b7efe5
EduardoL0pes <113311412+EduardoL0pes@users.noreply.github.com>
2024-09-19 13:45:05 -0300

    aula05 - desafios atualizados

--- Aula05/desafio01.py
+++ Aula05/desafio01.py
@@ -1,11 +1,13 @@
-nome = input('Digite seu nome completo: ')
+nome = input('Digite seu nome completo: ').strip()
 u = nome.upper()
 l = nome.lower()
-qt = len(nome.replace(' ', ''))
-pn = len(nome[0:6])
+qt = len(nome) - nome.count(' ')
+#qt = len(nome.replace(' ', '')) #uma forma de retirar os espaços entres as palavras
+#pn = nome.find(' ') #uma forma de mostrar a quantidade de caracteres do primeiro nome
+pn = nome.split()
 print("""
 Nome completo é: {}
 Letras maiusculas: {}
 Letras minusculas: {}
 Quantidade de letras: {}
-Qt de letras no primeiro nome: {}""" .format(nome, u, l, qt, pn))
+Qt de letras no primeiro nome: {}""" .format(nome, u, l, qt, len(pn[0])))
--- Aula05/desafio06.py
+++ Aula05/desafio06.py
@@ -1,7 +1,7 @@
 nome = input('Digite seu nome completo: ')
 div = nome.split()
-first = nome[0]
-last = nome[-1]
+first = div[0]
+last = div[-1]
 print("""Seu nome é: {}
 Primeiro: {}
 Ultimo: {} """.format(nome, first, last))
