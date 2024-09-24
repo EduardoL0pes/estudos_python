9f4555ec80ee4dec6e9334eff5709fcfbd3876ee
EduardoL0pes <113311412+EduardoL0pes@users.noreply.github.com>
2024-09-24 17:44:01 -0300

    aula06

--- Aula06/if_else.py
+++ Aula06/if_else.py
@@ -1,3 +1,8 @@
-nome = 'Era uma vez'
-nome = nome.split()
-print('-'.join(nome))
+n1 = float(input('Digite a primeira nota: '))
+n2 = float(input('Digite a segunda nota: '))
+m = (n1 + n2) / 2
+print('A sua média é {:.1f}'.format(m))
+if m >= 6.0:
+    print('Parabéns você passou no teste!')
+else:
+    print('Infelizmente você reprovou, mas não desista!')
