459bd70f2e13f48e37ef42ca77d69e0ad6dd4081
EduardoL0pes <113311412+EduardoL0pes@users.noreply.github.com>
2024-09-30 18:14:08 -0300

    aula06 - desafio01

--- Aula06/desafio01.py
+++ Aula06/desafio01.py
@@ -1,11 +1,15 @@
-import random
+from random import randint
+from time import sleep
 
-num = random.randint(0, 5)
-res = int(input('Digite o valor: '))
+num = randint(0, 5)
+print('Vou pensar em um número entre 0 e 5. Tente adivinhar...') #computador
+res = int(input('Digite o valor: ')) #jogador
+print('PROCESSANDO...') #tempo de processamento obs:somente estético
+sleep(2) #tempo de precessamento obs:somente estético
 if res == num:
     print('Parabéns você acertou!!')
 else:
     print('Você perdeu :p')
     print('A resposta certa é {}'.format(num))
 
-print('Fim de Jogo')
+print('Fim de Jogo...')
