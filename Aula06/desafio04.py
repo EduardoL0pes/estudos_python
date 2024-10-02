d83ef0080683a79a2834e95167f84467984d5a0c
EduardoL0pes <113311412+EduardoL0pes@users.noreply.github.com>
2024-10-02 18:17:55 -0300

    aula06 - desafios

--- Aula06/desafio04.py
+++ Aula06/desafio04.py
@@ -1,7 +1,10 @@
-distancia = float(input('Digite a distância percorrida: '))
+from time import sleep
+distancia = float(input('Digite a distância que irá percorrer: '))
+print('CALCULANDO DISTÂNCIA...')
+sleep(2)
 if distancia <= 200:
-    menor = distancia * 0.5
+    menor = distancia * 0.50
     print('A distância que irá percorrer/percorrida é de {:.0f}Km \n valor estimado para essa corrida é R${:.2f}'.format(distancia, menor))
 else:
-    maior = distancia * 0.4
+    maior = distancia * 0.45
     print('A distância que irá percorrer/percorrida é de {:.0f}Km \n valor estimado para essa corrida é de R${:.2f}'.format(distancia,maior))
