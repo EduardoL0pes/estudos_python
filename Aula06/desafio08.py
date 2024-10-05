9c440e31c1c510125878e590fedb7535aec703a3
EduardoL0pes <113311412+EduardoL0pes@users.noreply.github.com>
2024-10-05 08:35:28 -0300

    aula06 - desafios

--- Aula06/desafio08.py
+++ Aula06/desafio08.py
@@ -1,8 +1,16 @@
-r1 = int(input('Digite o primeiro valor: '))
-r2 = int(input('Digite o segundo valor: '))
-r3 = int(input('Digite o terceiro valor: '))
+from time import sleep
+
+r1 = float(input('Digite o primeiro valor: '))
+r2 = float(input('Digite o segundo valor: '))
+r3 = float(input('Digite o terceiro valor: '))
 soma = r1 + r2
+print('=-=-' * 10)
+print('Calculando Valores...')
+print('=-=-' * 10)
+sleep(2)
 if soma > r3:
-    print('Pode se formar um triângulo \n valor da soma de {} + {} = {} > {}'.format(r1, r2, soma, r3))
+    print('Valor da soma de {} + {} = {} se for > {} \n Podem se formar um triângulo '.format(r1, r2, soma, r3))
 else:
-    print('Não pode formar um triângulo \n valor da soma de {} + {} = {} < {}'.format(r1, r2, soma, r3))
+    print('valor da soma de {} + {} = {} se for < {} \n Não podem formar um triângulo'.format(r1, r2, soma, r3))
+
+"""OBS: O resultado da soma de r1 + r2 não pode ser menor que r3"""
