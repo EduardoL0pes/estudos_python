d5ecb55af3a5ba56d89f99791c5743236486200a
EduardoL0pes <113311412+EduardoL0pes@users.noreply.github.com>
2024-10-05 08:02:28 -0300

    aula06 - desafios

--- Aula06/desafio06.py
+++ Aula06/desafio06.py
@@ -1,4 +1,18 @@
 """Faça um programa que leia três números e mostre qual é o maior e qual é o menor"""
-num = input('Digite um número: ')
-
-print(num)
+n1 = int(input('Primeiro valor: '))
+n2 = int(input('Segundo valor: '))
+n3 = int(input('Terceiro valor: '))
+#Verificando menor valor
+menor = n1
+if n2 < n1 and n2 < n3:
+    menor = n2
+if n3 < n1 and n3 < n2:
+    menor = n3
+#Verificando maior valor
+maior = n1
+if n2 > n1 and n2 > n3:
+    maior = n2
+if n3 > n1 and n3 > n2:
+    maior = n3
+print('O menor valor é {}'.format(menor))
+print('O maior valor é {}'.format(maior))
