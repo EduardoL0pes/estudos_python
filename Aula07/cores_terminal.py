538b8c98ece74ed916e749d6cdaa425e8fa7d8b5
EduardoL0pes <113311412+EduardoL0pes@users.noreply.github.com>
2024-10-07 18:14:10 -0300

    aula07 - cores no terminal

--- Aula07/cores_terminal.py
+++ Aula07/cores_terminal.py
@@ -8,3 +8,10 @@ print('Os valores são \033[32m{}\033[m e \033[31m{}\033[m'.format(a, b))
 
 nome = 'Eduardo'
 print('Prazer em te conhecer, {}{}{}!!!'.format('\033[4;34m', nome, '\033[m'))
+
+cores = {'limpa': '\033[m',
+         'amarelo': '\033[33m',
+         'azul': '\033[34m',
+         'pretoebranco': '\033[7;40m'}
+
+print('Seja bem vindo, {}{}{}!!'.format(cores['pretoebranco'], nome, cores['limpa']))
