70d0651084d1512e19b0ee93c3fdcc547eeb838b
EduardoL0pes <113311412+EduardoL0pes@users.noreply.github.com>
2024-10-07 18:07:37 -0300

    aula07 - cores no terminal

--- Aula07/cores_terminal.py
+++ Aula07/cores_terminal.py
@@ -1,3 +1,10 @@
 print('\033[1;7;40mHello, World!\033[m')
 print('\033[1;36;40mTestando\033[m')
 print('\033[7;31;44m=-=-\033[m'*10)
+
+a = 3
+b = 5
+print('Os valores são \033[32m{}\033[m e \033[31m{}\033[m'.format(a, b))
+
+nome = 'Eduardo'
+print('Prazer em te conhecer, {}{}{}!!!'.format('\033[4;34m', nome, '\033[m'))
