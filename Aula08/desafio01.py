f85a54a1d033ed33497e01038bcc24f0c947e422
EduardoL0pes <113311412+EduardoL0pes@users.noreply.github.com>
2024-10-14 17:44:05 -0300

    aula08 - desafio01

---
+++ Aula08/desafio01.py
@@ -0,0 +1,5 @@
+print('\033[1;33mPor favor para confirmar sua aprovação de empréstimo me passe as seguintes informações para fazermos o calculo:\033[m')
+emprestimo = float(input('Valor do empréstimo: '))
+salario = float(input('Seu Salário: '))
+parcela = int(input('Quantos anos para pagar: '))
+
