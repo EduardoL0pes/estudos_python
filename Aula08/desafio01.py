5aceaa2ec3f1dbaf5062472c11a5b858ce516fbe
EduardoL0pes <113311412+EduardoL0pes@users.noreply.github.com>
2024-10-15 17:57:57 -0300

    aula08 - desafio01

--- Aula08/desafio01.py
+++ Aula08/desafio01.py
@@ -3,3 +3,10 @@ emprestimo = float(input('Valor do empréstimo: '))
 salario = float(input('Seu Salário: '))
 parcela = int(input('Quantos anos para pagar: '))
 
+div = emprestimo / parcela
+exceder = salario * 30 / 100
+
+if exceder > div:
+    print('\033[1;32mSeu empréstimo foi aprovado!\033[m')
+else:
+    print('\033[1;31mSeu empréstimo foi reprovado\033[m')
