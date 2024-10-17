c8e0e03d8124175e0eb2698b6b4962407ebacf2f
EduardoL0pes <113311412+EduardoL0pes@users.noreply.github.com>
2024-10-17 17:28:35 -0300

    aula08 - desafio03

--- Aula08/desafio03.py
+++ Aula08/desafio03.py
@@ -1,2 +1,9 @@
 n1 = int(input('Digite um número: '))
 n2 = int(input('Digite outro número: '))
+
+if n1 > n2:
+    print('O número {} é maior.'.format(n1))
+elif n2 > n1:
+    print('O número {} é maior.'.format(n2))
+elif n1 == n2:
+    print('Não existe valor maior, os dois são iguais.')
