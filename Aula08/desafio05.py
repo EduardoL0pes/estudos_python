b1298295c99b66db19c5d0e542e0f15e80da1775
EduardoL0pes <113311412+EduardoL0pes@users.noreply.github.com>
2024-10-20 11:27:51 -0300

    aula08 - desafio05

--- Aula08/desafio05.py
+++ Aula08/desafio05.py
@@ -4,7 +4,7 @@ res = (nota1 + nota2) / 2
 
 if res <= 4.9:
     print('Sua média final é \033[1;31m{}\033[m, você foi \033[1;31mREPROVADO!\033[m'.format(res))
-elif res == 5 and 6.9:
+elif res >= 5  <= 6.9:
     print('Sua média final é \033[1;33m{}\033[m, você foi para \033[1;33mRECUPERAÇÃO\033[m'.format(res))
 elif res >= 7:
     print('Sua média final é \033[1;32m{}\033[m, PARABÉNS VOCÊ FOI \033[1;32mAPROVADO!!!\033[m'.format(res))
