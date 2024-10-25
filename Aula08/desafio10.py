ee137ee5ca655c369c125d088d1eb118e4f9ab68
EduardoL0pes <113311412+EduardoL0pes@users.noreply.github.com>
2024-10-25 14:13:07 -0300

    aula08 - desafio10

--- Aula08/desafio10.py
+++ Aula08/desafio10.py
@@ -5,7 +5,4 @@ print('-=-='*10)
 jogador = input('Digite sua escolha: ')
 jokenpo = 'pedra', 'papel', 'tesoura'
 gerador = choice(jokenpo)
-if jogador == gerador:
-    print('Parabéns você VENCEU!!')
-else:
-    print('Computador venceu, \033[1;31m{}\033[m ganha de \033[1;31m{}\033[m.'.format(gerador, jogador))
+
