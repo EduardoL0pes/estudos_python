05413fc372cd97f3d6029476b61e5a04d9da9d29
EduardoL0pes <113311412+EduardoL0pes@users.noreply.github.com>
2024-10-26 15:01:29 -0300

    aula08 - desafio02

--- Aula08/desafio02.py
+++ Aula08/desafio02.py
@@ -1,1 +1,14 @@
-#desafio037 coming soon...
+num  = int(input('Digite um valor: '))
+print('''Escolha uma das bases para conversão: 
+[ 1 ] converter para BINÁRIO
+[ 2 ] converter para OCTAL
+[ 3 ] converter para HEXADECIMAL''')
+opcao = int(input('Sua opção: '))
+if opcao == 1:
+    print('{} convertido para BINÁRIO é igual á {}'.format(num, bin(num)[2:])) #[2:] para omitir o prefixo 0b
+elif opcao == 2:
+    print('{} convertido para OCTAL é igual á {}'.format(num, oct(num)[2:]))
+elif opcao == 3:
+    print('{} convertido para HEXADECIMAL é igual á {}'.format(num, hex(num)[2:]))
+else:
+    print('Opção inválida, tente novamente.')
