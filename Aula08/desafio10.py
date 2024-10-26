55abdb93b62db34687f13794d126bd9c218245f4
EduardoL0pes <113311412+EduardoL0pes@users.noreply.github.com>
2024-10-26 10:36:38 -0300

    aula08 - desafio10

--- Aula08/desafio10.py
+++ Aula08/desafio10.py
@@ -5,4 +5,15 @@ print('-=-='*10)
 jogador = input('Digite sua escolha: ')
 jokenpo = 'pedra', 'papel', 'tesoura'
 gerador = choice(jokenpo)
-
+if gerador == jogador:
+    print('EMPATE! Por favor tente novamente! {} DRAW {}'.format(jogador, gerador))
+    print('\033[1;33mFIM DE JOGO!!!\033[m')
+elif jogador == 'papel' and gerador == 'pedra':
+    print('Parabéns você venceu! {} ganha de {}'.format(jogador, gerador))
+elif jogador == 'pedra' and gerador == 'tesoura':
+    print('Parabéns você venceu! {} ganha de {}'.format(jogador, gerador))
+elif jogador == 'tesoura' and gerador == 'papel':
+    print('Parabéns você venceu! {} ganha de {}'.format(jogador, gerador))
+else:
+    print('Você perdeu :/ {} ganha de {}'.format(gerador, jogador))
+    print('\033[1;33mFIM DE JOGO!!!\033[m')
