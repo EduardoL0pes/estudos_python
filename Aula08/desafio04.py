559a30a6e064b5dbed3e69f8dce52b760c24b6df
EduardoL0pes <113311412+EduardoL0pes@users.noreply.github.com>
2024-10-27 08:38:02 -0300

    aula08 - desafio04

--- Aula08/desafio04.py
+++ Aula08/desafio04.py
@@ -3,8 +3,8 @@ from datetime import date
 ano = int(input('Digite seu ano de nascimento: '))
 calc = date.today().year - ano
 if calc <= 17:
-    falta = 18 - calc
-    print('Você tem atualmente {} anos, ainda não tem idade para se alistar, faltam {} anos'.format(calc, falta))
+    falta = ano + 18
+    print('Você tem atualmente {} anos, ainda não tem idade para se alistar, você deverá se alistar em {}'.format(calc, falta))
 elif calc == 18:
     print('Você tem {} anos já é hora de se alistar'.format(calc))
 elif calc >= 19:
