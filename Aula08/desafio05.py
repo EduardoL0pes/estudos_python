cac4f7ff4d15071d556e85f0ec9e29bfa51fc473
EduardoL0pes <113311412+EduardoL0pes@users.noreply.github.com>
2024-10-27 09:42:43 -0300

    aula08 - desafio05

--- Aula08/desafio05.py
+++ Aula08/desafio05.py
@@ -3,8 +3,8 @@ nota2 = float(input('Digite a sua segunda nota: '))
 res = (nota1 + nota2) / 2
 
 if res <= 4.9:
-    print('Sua média final é \033[1;31m{}\033[m, você foi \033[1;31mREPROVADO!\033[m'.format(res))
-elif 5 == res <= 6.9:
-    print('Sua média final é \033[1;33m{}\033[m, você foi para \033[1;33mRECUPERAÇÃO\033[m'.format(res))
+    print('Sua média final é \033[1;31m{:.1f}\033[m, aluno foi \033[1;31mREPROVADO.\033[m'.format(res))
+elif 5 <= res < 7:
+    print('Sua média final é \033[1;33m{:.1f}\033[m, aluno foi para \033[1;33mRECUPERAÇÃO.\033[m'.format(res))
 elif res >= 7:
-    print('Sua média final é \033[1;32m{}\033[m, PARABÉNS VOCÊ FOI \033[1;32mAPROVADO!!!\033[m'.format(res))
+    print('Sua média final é \033[1;32m{:.1f}\033[m, PARABÉNS O ALUNO FOI \033[1;32mAPROVADO.\033[m'.format(res))
