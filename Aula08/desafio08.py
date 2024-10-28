0591f56c0fc3f6500ae5650c903875e5e9de99bc
EduardoL0pes <113311412+EduardoL0pes@users.noreply.github.com>
2024-10-28 14:07:19 -0300

    aula08 - desafio08

--- Aula08/desafio08.py
+++ Aula08/desafio08.py
@@ -6,17 +6,17 @@ print('\033[31m-=-=\033[m'*10)
 
 peso = int(input('Digite seu peso: '))
 altura = float(input('Digite sua altura: '))
-calc = round(peso / (altura * altura), 2)
+calc = round(peso / (altura ** 2), 1)
 print('\033[1;32mCALCULANDO IMC...\033[m')
 sleep(2)
 print(calc)
-if calc <= 18.5:
+if calc < 18.5:
     print('Você está abaixo do peso')
-elif 18.5 < calc <= 25:
+elif 18.5 <= calc < 25:
     print('Você está no peso ideal')
-elif 25 < calc <= 30:
+elif 25 <= calc < 30:
     print('Sobrepeso')
-elif 30 < calc <= 40:
+elif 30 <= calc < 40:
     print('Obesidade')
 else:
     print('Obesidade Mórbida')
