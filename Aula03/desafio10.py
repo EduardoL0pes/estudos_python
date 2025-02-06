b5accf9f1fca13d46f1f5bf6246e952a3e171e54
Eduardo Lopes <113311412+EduardoL0pes@users.noreply.github.com>
2025-02-06 15:31:57 -0300

    Update desafio10.py

--- Aula03/desafio10.py
+++ Aula03/desafio10.py
@@ -1,6 +1,6 @@
-f = float(input('Digite a temperatura em Fahrenheit: '))
+f = float(input('Informe a temperatura em Fahrenheit: '))
 cel = (f - 32) / 1.8
-celsius = float(input('Digite a temperatura em Celsius: '))
+celsius = float(input('Informe a temperatura em Celsius: '))
 fah = celsius * 1.8 + 32
-print('A temperatura em Fahrenheit é de {:.0f}°F, convertendo para Celsius fica {:.0f}°C'.format(f, cel))
-print('A temperatura em Celsius é {:.0f}°C, convertendo para Fahrenheit fica {:.0f}°F'.format(celsius, fah))
+print('A temperatura de {:.0f}°F corresponde a {:.0f}°C'.format(f, cel))
+print('A temperatura de {:.0f}°C corresponde a {:.0f}°F'.format(celsius, fah))
