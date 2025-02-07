89bfdcd04b1fc18af7680b4fb437a6f398b8a5bd
Eduardo Lopes <113311412+EduardoL0pes@users.noreply.github.com>
2025-02-07 16:44:37 -0300

    Update desafio11.py

--- Aula03/desafio11.py
+++ Aula03/desafio11.py
@@ -1,6 +1,4 @@
 diaria = int(input('Quantos dias alugados? '))
 km = float(input('Quantos km rodados? '))
-carro = diaria * 60
-kmrodado = km * 0.15
-total = carro + kmrodado
+total = (diaria * 60) + (kmrodado * 0.15)
 print('Valor diário R${:.2f}, Km rodado R${:.2f}, Total a pagar R${:.2f}'.format(carro, kmrodado, total))
