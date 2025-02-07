be6d0d728e1b516e77fd5cc38b53e5cc1fb84460
Eduardo Lopes <113311412+EduardoL0pes@users.noreply.github.com>
2025-02-07 16:54:11 -0300

    Update desafio11.py

--- Aula03/desafio11.py
+++ Aula03/desafio11.py
@@ -3,4 +3,5 @@ km = float(input('Quantos km rodados? '))
 carro = diaria * 60
 kmrodado = km * 0.15
 total = carro + kmrodado
-print('Valor diário R${:.2f}, Km rodado R${:.2f}, Total a pagar R${:.2f}'.format(carro, kmrodado, total))
+#print('Valor diário R${:.2f}, Km rodado R${:.2f}, Total a pagar R${:.2f}'.format(carro, kmrodado, total))
+print(f'Valor da diária R${carro}, valor por km rodado R${kmrodado:.2f}, total a pagar R${total}')
