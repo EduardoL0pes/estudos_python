e74da5fc0b833cec58ac039342286cc667369e13
Eduardo Lopes <113311412+EduardoL0pes@users.noreply.github.com>
2025-02-08 12:40:51 -0300

    Update desafio01.py

--- Aula04/desafio01.py
+++ Aula04/desafio01.py
@@ -1,4 +1,7 @@
 from math import trunc
 num = float(input('Digite um número: '))
-print('O número {} tem a parte inteira {}'.format(num, trunc(num)))
+print(f'O número {num} tem aparte inteira {trunc(num)}')
+#print('O número {} tem a parte inteira {}'.format(num, trunc(num)))
+
 #podendo usar tbm no lugar do TRUNC a função int(), com isso não irá precisar importa a biblioteca math
+#print(f'O numero {num} tem a parte inteira {int(num)}')
