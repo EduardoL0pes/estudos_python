c04ae6794f6e3e417cef330b1845a00685e8e6f5
Eduardo Lopes <113311412+EduardoL0pes@users.noreply.github.com>
2025-02-11 17:14:19 -0300

    Update desafio01.py

--- Aula05/desafio01.py
+++ Aula05/desafio01.py
@@ -17,10 +17,12 @@ Qt de letras no primeiro nome: {}""" .format(nome, u, l, qt, len(pn[0])))
 nome = input('Digite seu nome completo: ').strip()
 print(f"""
 Nome completo: {nome}
-Qt de caracteres: {len(nome)}
+Qt. de caracteres: {len(nome)}
 Letras Minúsculas: {nome.lower()}
 Letras Maiúsculas: {nome.upper()}
 Nome sem espaços:  {nome.replace(' ', '')}
 Qt. de caracteres sem os espaços: {len(nome.replace(' ', ''))}
-Qt de caracteres primeiro nome: {len(nome.split()[0])}
+Qt. de caracteres primeiro nome: {len(nome.split()[0])} 
 """)
+#{len(nome.split()[0])} #analisa o comprimento do nome len(), coloca em lista nome.split(), mostra a qt. de caracter do primeiro nome da lista [0] 
+#Qt. de caracteres primeiro nome: {nome.find(' ')}  #Outra forma de identificar a qt de caracteres do primeiro nome
