caa7ee1a5fbd301a903f5c9246e3114bb8362c65
Eduardo Lopes <113311412+EduardoL0pes@users.noreply.github.com>
2025-02-12 14:39:12 -0300

    Update desafio03.py

--- Aula05/desafio03.py
+++ Aula05/desafio03.py
@@ -1,3 +1,4 @@
-nome = input('Digite o nome da sua cidade: ').strip().upper()
-print('Resposta é: '.format(nome), 'SANTO' in nome)
+cidade = input('Digite o nome da sua cidade: ').strip().upper()
+print(cidade[:5] == 'SANTO') #Verifica se a cidade começa com a palavra 'santo' 
+#print('Resposta é: '.format(cidade), 'SANTO' in cidade)
 
