369200c207977babd4646086374956364808d829
Eduardo Lopes <113311412+EduardoL0pes@users.noreply.github.com>
2025-02-12 16:07:12 -0300

    Update desafio05.py

--- Aula05/desafio05.py
+++ Aula05/desafio05.py
@@ -1,3 +1,4 @@
+#Forma 1
 frase = input('Digite uma frase: ').strip().lower()
 e = frase.count('e')
 first = frase.find('e')
@@ -6,3 +7,9 @@ print("""
 Quantidade de letras 'e' é: {}
 Primeira letra 'e' fica na posição: {} 
 Ultima letra 'e' fica na posição: {}""".format(e, first, last))
+
+#Forma 2
+"""frase = input('Digite uma frase qualquer: ').strip().lower()
+print('Qt. letras "A" na frase: ', frase.count('a'))
+print('Aparece a primeira vez na posição: ', frase.find('a'))
+print('Aparece pela última vez na posição: ', frase.rfind('a'))"""
