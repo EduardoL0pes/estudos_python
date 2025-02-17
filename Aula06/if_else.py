38c7f24d36917a74dcd5198ff9576e5eb46e5af4
Eduardo Lopes <113311412+EduardoL0pes@users.noreply.github.com>
2025-02-17 18:38:07 -0300

    Update if_else.py

--- Aula06/if_else.py
+++ Aula06/if_else.py
@@ -1,8 +1,9 @@
 n1 = float(input('Digite a primeira nota: '))
 n2 = float(input('Digite a segunda nota: '))
 m = (n1 + n2) / 2
-print('A sua média é {:.1f}'.format(m))
-if m >= 6.0:
+print(f'Parabéns você foi aprovado! \n Média final: {m}' if m>=6 else 'Infelizmente você foi reprovado.') #Forma mais simplificada, para condições mais simples
+
+"""if m >= 6.0:
     print('Parabéns você passou no teste!')
 else:
-    print('Infelizmente você reprovou, mas não desista!')
+    print('Infelizmente você reprovou, mas não desista!')"""
