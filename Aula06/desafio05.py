d03bbd95c0159383095777c4a5090fd9f8feb6e1
Eduardo Lopes <113311412+EduardoL0pes@users.noreply.github.com>
2025-02-25 18:12:38 -0300

    Update desafio05.py

--- Aula06/desafio05.py
+++ Aula06/desafio05.py
@@ -1,7 +1,7 @@
 from datetime import date
 ano = int(input('Que ano deseja analisar? Coloque 0 para analisar o ano atual: '))
 if ano == 0:
-    ano = date.today().year
+    ano = date.today().year #analisa ano atual da máquina
 if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
     print('Ano {} é bissexto'.format(ano))
 else:
