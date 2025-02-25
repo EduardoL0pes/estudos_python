bc7ac9fa87197b85a7ec9febc19a7a388e5f88b4
Eduardo Lopes <113311412+EduardoL0pes@users.noreply.github.com>
2025-02-25 17:26:05 -0300

    Update desafio01.py

--- Aula06/desafio01.py
+++ Aula06/desafio01.py
@@ -3,9 +3,9 @@ from time import sleep
 
 num = randint(0, 5)
 print('Vou pensar em um número entre 0 e 5. Tente adivinhar...') #computador
-res = int(input('Digite o valor: ')) #jogador
+res = int(input('Digite seu palpite: ')) #jogador
 print('PROCESSANDO...') #tempo de processamento obs:somente estético
-sleep(2) #tempo de processamento obs:somente estético
+sleep(2) #tempo de processamento (2sec)
 if res == num:
     print('Parabéns você acertou!!')
 else:
