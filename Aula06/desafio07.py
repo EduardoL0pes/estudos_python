26cdb6646deeeda76367514bda122309a0190a38
Eduardo Lopes <113311412+EduardoL0pes@users.noreply.github.com>
2025-02-25 18:30:26 -0300

    Update desafio07.py

--- Aula06/desafio07.py
+++ Aula06/desafio07.py
@@ -1,9 +1,9 @@
 salario = float(input('Digite seu salário: R$'))
 if salario >= 1250:
-    sup = salario * 10 / 100 #Salários superiores a R$1.250
+    sup = salario * 10 / 100 #Salário superior a R$1.250
     novo = salario + sup
     print('Seu novo salário com 10% de aumento fica R${:.2f} com um aumento de R${:.2f}'.format(novo, sup))
 else:
-    inf = salario * 15 / 100 #Salários inferiores a R$1.000
-    novovalor = salario + inf
-    print('Seu novo salário com 15% de aumento fica R${:.2f} com um aumento de R${:.2f}'.format(novovalor, inf))
+    inf = salario * 15 / 100 #Salário inferior a R$1.250
+    novo = salario + inf
+    print('Seu novo salário com 15% de aumento fica R${:.2f} com um aumento de R${:.2f}'.format(novo, inf))
