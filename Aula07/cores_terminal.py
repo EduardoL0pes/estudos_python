1521140b748c14f1ce5a4392fad5be18ac155f7f
Eduardo Lopes <113311412+EduardoL0pes@users.noreply.github.com>
2025-02-26 18:26:39 -0300

    Update cores_terminal.py

--- Aula07/cores_terminal.py
+++ Aula07/cores_terminal.py
@@ -5,7 +5,6 @@
    7 - negative
    \033[0;33;44m]"""
 
-
 print('\033[1;7;40mHello, World!\033[m')
 print('\033[1;36;40mTestando\033[m')  #part 01
 print('\033[7;31;44m=-=-\033[m'*10)
@@ -15,7 +14,9 @@ b = 5
 print('Os valores são \033[32m{}\033[m e \033[31m{}\033[m'.format(a, b))  #part02
 
 nome = 'Eduardo'
-print('Prazer em te conhecer, {}{}{}!!!'.format('\033[4;34m', nome, '\033[m')) #part03
+print(f'Prazer em te conhecer, {'\033[4;34m'}{nome}{'\033[m'}!!')
+#print('Prazer em te conhecer, {}{}{}!!!'.format('\033[4;34m', nome, '\033[m')) #part03
+
 
 cores = {'limpa': '\033[m',
          'amarelo': '\033[33m',   #part04
