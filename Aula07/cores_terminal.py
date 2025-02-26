637976e3a2fcc140ee7e851b122628451dac0db2
Eduardo Lopes <113311412+EduardoL0pes@users.noreply.github.com>
2025-02-26 18:01:38 -0300

    Update cores_terminal.py

--- Aula07/cores_terminal.py
+++ Aula07/cores_terminal.py
@@ -2,7 +2,9 @@
    0 - none          30      40
    1 - bold           á       á
    4 - Underline     31      47
-   7 - negative"""
+   7 - negative
+   \033[0;33;44m]"""
+
 
 print('\033[1;7;40mHello, World!\033[m')
 print('\033[1;36;40mTestando\033[m')  #part 01
