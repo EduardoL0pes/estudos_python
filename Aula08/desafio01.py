87d6893f7d31415616e7390dcf800bbbe65376b0
Eduardo Lopes <113311412+EduardoL0pes@users.noreply.github.com>
2025-03-03 16:47:07 -0300

    Update desafio01.py

--- Aula08/desafio01.py
+++ Aula08/desafio01.py
@@ -1,4 +1,5 @@
-from time import sleep
+#Opção 01 SEM ENTRADA
+"""from time import sleep
 
 print('\033[1;33mPor favor para confirmar sua aprovação de empréstimo me passe as seguintes informações para fazermos o calculo:\033[m')
 emprestimo = float(input('Valor do empréstimo: R$'))
@@ -19,4 +20,28 @@ print('-=-='*10)
 print('Simulação do empréstimo: '
       '\nValor do empréstimo: R${:.2f} '
       '\nSalário do comprador: R${:.2f} '
-      '\nValor das parcelas: R${:.2f} '.format(emprestimo, salario, parcelas))
+      '\nValor das parcelas: R${:.2f} '.format(emprestimo, salario, parcelas))"""
+
+#Opção 02 COM ENTRADA
+from time import sleep
+
+casa = float(input('Digite o valor da casa: '))
+salario = float(input('Seu salário: '))
+parcela = int(input('Quantos anos de financiamento? '))
+entrada = int(input('Valor de entrada: '))
+print('\033[1;33mCALCULANDO EMPRÉSTIMO, POR FAVOR AGUARDE...\033[m')
+sleep(2)
+calc_parcela = casa / parcela / 12
+calc_salario = salario * 30 / 100
+if calc_parcela >= calc_salario and entrada == 0:
+    print('Empréstimo \033[1;31mNEGADO\033[m!')
+elif calc_salario >= calc_parcela and entrada == 0:
+    print('Empréstimo \033[1;32APROVADO\033[m!')
+elif entrada == entrada and entrada > 0:
+    novo_calc_casa = casa - entrada
+    novo_calc_parcela = novo_calc_casa / parcela / 12
+    novo_calc_salario = salario * 30 / 100
+    if novo_calc_parcela >= novo_calc_salario:
+        print('Empréstimo \033[1;31mCOM ENTRADA NEGADO\033[m!')
+    else:
+        print('Empréstimo \033[1;32mCOM ENTRADA APROVADO\033[m!')
