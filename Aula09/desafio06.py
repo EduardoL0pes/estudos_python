0bfc238e9d2414bb5d03f5afc8ebd437ed976cb7
Eduardo <eduardo_lopes010@hotmail.com>
2025-04-03 17:23:12 -0300

    Desafio 06, 07

---
+++ Aula09/desafio06.py
@@ -0,0 +1,5 @@
+termo = int(input('Primeiro termo: '))
+termo_final = 10
+razao = int(input('Razão: '))
+for c in range(termo, termo_final+1, razao):
+    print(c)
---
+++ Aula09/desafio07.py
@@ -0,0 +1,5 @@
+for c in range(1, 2):
+    num = int(input('Digite um número: '))
+    if num / 1 == num and num % num == 0:
+        print(f'O número {num} é primo.')
+
