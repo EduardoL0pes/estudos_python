9b6ae29b0b0426cb2d334dfa320f2c4545799312
Eduardo <eduardo_lopes010@hotmail.com>
2025-04-06 00:30:09 -0300

    Desafio 07

--- Aula09/desafio07.py
+++ Aula09/desafio07.py
@@ -1,5 +1,7 @@
-for c in range(1, 2):
+for c in range(2, 1):
     num = int(input('Digite um número: '))
     if num / 1 == num and num % num == 0:
         print(f'O número {num} é primo.')
+    else:
+        print(f'O número {num} não é primo')
 
