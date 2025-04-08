d252184c1d7b9ea8f963c4dd74b14566451e6eb0
Eduardo <eduardo_lopes010@hotmail.com>
2025-04-08 14:51:51 -0300

    Desafio 08

--- Aula09/desafio08.py
+++ Aula09/desafio08.py
@@ -1,5 +1,5 @@
 texto = input('Escreva uma frase: ')
-#Remove espaços e converte para minúsculas
+#Remove espaços, verifica se é alfanúmerico e converte para minúsculas
 texto_limpo = ''.join(c for c in texto if c.isalnum()).lower()
 #verifica se o texto é igual ao seu reverso
 if texto_limpo == texto_limpo[:: -1]:
