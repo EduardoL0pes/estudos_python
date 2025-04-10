9e45821ec89c7a7a06cd85b893ca4fcd1e57e740
Eduardo <eduardo_lopes010@hotmail.com>
2025-04-10 16:21:08 -0300

    Desafio 01

---
+++ Aula09/desafio11.py
@@ -0,0 +1,10 @@
+media_idade = 0
+homem_velho = 0
+mulher = 0
+for c in range(2):
+    nome = input('Seu nome: ')
+    idade = int(input('Idade: '))
+    sexo = input('Seu sexo: ')
+    if idade > 1:
+        media_idade = idade
+        
