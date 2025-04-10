a54c3bcde399ff6ce9170515d240dbc7ca5177cb
Eduardo <eduardo_lopes010@hotmail.com>
2025-04-10 17:00:10 -0300

    Desafio 04

--- Aula09/desafio04.py
+++ Aula09/desafio04.py
@@ -1,7 +1,13 @@
 print('Tabuada 2.0')
-tb = int(input('Tabuada de qual número?: '))
+#Opção I
+"""tb = int(input('Tabuada de qual número?: '))
 n = 0
-for c in range(1, 10+1):
+for c in range(1, 11):
     c *= tb
     n += 1
-    print(f'{tb}x{n} = {c}')
+    print(f'{tb}x{n} = {c}')"""
+
+#Opção II
+tb = int(input('Digite um número para ver sua tabuada: '))
+for c in range(1,11):
+    print(f'{tb}x{c} = {tb*c}')
