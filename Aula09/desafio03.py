1938fe4572cab2913d5f9ebdd39a8ba121bd06e0
Eduardo <eduardo_lopes010@hotmail.com>
2025-04-10 16:47:41 -0300

    Desafio 03

--- Aula09/desafio03.py
+++ Aula09/desafio03.py
@@ -1,5 +1,16 @@
-soma = 0
-for c in range(1, 500):
+#Opção I - Valores ímpares multiplos de 3
+"""soma = 0
+for c in range(1, 501):
     if c % 3 == 0 and c % 2 != 0:
         soma += c
-print(soma)
+print(soma)"""
+
+#Opação II
+soma = 0
+cont = 0
+for c in range(1, 501, 2):
+    if c % 3 == 0:
+        soma += c
+    #cont += 1 #Todos os valores ímpares dentro da contagem entre 1 - 501
+        cont += 1 #Todos os valores ímpares que são múltiplos de 3
+print(f'A soma de todos os \033[1:35m{cont}\033[m valores solicitados é de \033[1:35m{soma}\033[m')
