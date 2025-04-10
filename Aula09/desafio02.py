8284239dd3c3b7ecf156648180e9d838ad613bae
Eduardo <eduardo_lopes010@hotmail.com>
2025-04-10 16:32:59 -0300

    Desafio 02

--- Aula09/desafio02.py
+++ Aula09/desafio02.py
@@ -1,3 +1,8 @@
-for c in range(1, 20):
+#Opção I - Todos os números pares entre 1 - 50
+for c in range(2,51,2):
+    print(c, end=' ')
+
+#Opção II - Todos os números pares entre 1 - 50
+"""for c in range(1, 51):
     if c % 2 == 0:
-        print(c)
+        print(c, end=' ')"""
