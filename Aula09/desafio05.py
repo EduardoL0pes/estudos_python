d718ecebd8760b108165acdff418b1ac6095b6bc
Eduardo <eduardo_lopes010@hotmail.com>
2025-04-11 13:01:02 -0300

    Desafio 05

--- Aula09/desafio05.py
+++ Aula09/desafio05.py
@@ -1,6 +1,18 @@
-soma = 0
-for c in range(1, 6+1):
+#Opção I
+"""soma = 0
+for c in range(1, 7):
     num = int(input('Digite um número: '))
     if num % 2 == 0:
         soma += num
-print(f'A soma de todos os valores pares é igual a {soma}')
+print(f'A soma de todos os valores pares é igual a {soma}')"""
+
+
+#Opção II
+soma = 0
+cont = 0
+for c in range(1, 7):
+    num = int(input(f'Digite o {c}° valor: '))
+    if num % 2 == 0:
+        soma += num
+        cont += 1
+print(f'Você informou {cont} números PARES e a soma foi {soma}')
