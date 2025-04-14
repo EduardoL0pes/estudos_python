7893c703acf6ab439f29e00faf546ab669005379
Eduardo <eduardo_lopes010@hotmail.com>
2025-04-14 15:17:39 -0300

    Desafio 07

--- Aula09/desafio07.py
+++ Aula09/desafio07.py
@@ -1,7 +1,14 @@
-for c in range(2, 1):
-    num = int(input('Digite um número: '))
-    if num / 1 == num and num % num == 0:
-        print(f'O número {num} é primo.')
+num = int(input('Digite um número: '))
+tot = 0
+for c in range(1, num + 1):
+    if num % c == 0:
+        print('\033[33m', end=' ')
+        tot += 1
     else:
-        print(f'O número {num} não é primo')
-
+        print('\033[31m', end=' ')
+    print(f'{c}', end=' ')
+print(f'\n\033[mO número {num} foi divisível {tot} vezes')
+if tot == 2:
+    print('E por isso ele É PRIMO!')
+else:
+    print('E por isso ele NÃO É PRIMO!')
