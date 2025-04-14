ef163787a1f7ec6a9afe776a1e0723b0613b0231
Eduardo <eduardo_lopes010@hotmail.com>
2025-04-14 15:38:48 -0300

    Desafio 08

--- Aula09/desafio08.py
+++ Aula09/desafio08.py
@@ -4,5 +4,7 @@ texto_limpo = ''.join(c for c in texto if c.isalnum()).lower()
 #verifica se o texto é igual ao seu reverso
 if texto_limpo == texto_limpo[:: -1]:
     print(f'A frase \033[1:32m{texto}\033[m é um palíndromo.')
+    print(f'Resultado fica assim: \033[1:32m{texto_limpo}\033[m')
 else:
     print('Não é um palíndromo.')
+    print(texto_limpo[::-1])
