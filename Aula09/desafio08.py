ca4b3092b387209339b771f69dda119cab500aaa
Eduardo <eduardo_lopes010@hotmail.com>
2025-04-14 15:55:21 -0300

    Desafio 08 com FOR

--- Aula09/desafio08.py
+++ Aula09/desafio08.py
@@ -1,4 +1,4 @@
-texto = input('Escreva uma frase: ')
+"""texto = input('Escreva uma frase: ')
 #Remove espaços, verifica se é alfanúmerico e converte para minúsculas
 texto_limpo = ''.join(c for c in texto if c.isalnum()).lower()
 #verifica se o texto é igual ao seu reverso
@@ -7,4 +7,17 @@ if texto_limpo == texto_limpo[:: -1]:
     print(f'Resultado fica assim: \033[1:32m{texto_limpo}\033[m')
 else:
     print('Não é um palíndromo.')
-    print(texto_limpo[::-1])
+    print(texto_limpo[::-1])"""
+
+#Utilizando laço FOR
+frase = input('Digite uma frase: ').strip().upper()
+palavras = frase.split()
+junto = ''.join(palavras)
+inverso = ''
+for letra in range(len(junto) -1, -1, -1):
+    inverso += junto[letra]
+print(f'O inverso de {junto} é {inverso}')
+if inverso == junto:
+    print('Temos um palíndromo!')
+else:
+    print('A frase digitada não é um palíndromo!')
