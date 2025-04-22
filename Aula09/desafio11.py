2aa0c2058085a4e17be0a3c7c72abbf340c3e99f
Eduardo <eduardo_lopes010@hotmail.com>
2025-04-22 16:52:05 -0300

    Desafio 11

--- Aula09/desafio11.py
+++ Aula09/desafio11.py
@@ -1,10 +1,23 @@
-media_idade = 0
-homem_velho = 0
-mulher = 0
-for c in range(2):
-    nome = input('Seu nome: ')
+somaidade = 0
+mediaidade = 0
+maioridadehomem = 0
+homemvelho = ''
+totmulher20 = 0
+for c in range(4):
+    print(f'----- {c + 1}ª PESSOA -----')
+    nome = input('Qual seu nome? ').strip().title()
     idade = int(input('Idade: '))
-    sexo = input('Seu sexo: ')
-    if idade > 1:
-        media_idade = idade
-        
+    sexo = input('Sexo [M/F]: ').strip()
+    somaidade += idade
+    if c == 0 and sexo in 'Mn': #Verifica se ao colocar o Sexo foi em letra Minuscula ou maiuscula
+        maioridadehomem = idade
+        homemvelho = nome
+    if sexo in 'Mn' and idade > maioridadehomem:
+        maioridadehomem = idade
+        homemvelho = nome
+    if sexo in 'Ff' and idade < 20:
+        totmulher20 += 1
+mediaidade = somaidade / 4
+print(f'A média de idade do grupo é de {mediaidade:.0f} anos.')
+print(f'O homem mais velho tem {maioridadehomem} anos e se chama {homemvelho}.')
+print(f'Ao todo são {totmulher20} mulheres com menos de 20 anos.')
