add8cfd53d13a250d2e36190a72060423682c05a
Eduardo <eduardo_lopes010@hotmail.com>
2025-05-20 17:24:27 -0300

    Aula 10 - Desafio02 finalizado

--- Aula10/desafio02.py
+++ Aula10/desafio02.py
@@ -6,7 +6,6 @@ palpites = 0
 print('\033[1;33m----Jogo de Adivinha----\033[m')
 while num == '':
     num = randint(1,10)
-    print(num)
     palpite = int(input('Digite um número entre 0 á 10: '))
     if num == palpite:
         print('Parabéns você \033[1;32mACERTOU!\033[m \nNossa.. e de primeira, Sortudo!!')
