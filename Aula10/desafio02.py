db01aafba838baf2e571e24946850f6a8621fe58
Eduardo <eduardo_lopes010@hotmail.com>
2025-08-04 12:47:57 -0300

    Aula 10 - Desafio02.

--- Aula10/desafio02.py
+++ Aula10/desafio02.py
@@ -2,7 +2,7 @@ from random import randint
 
 num = ''
 palpite = ''
-palpites = 0
+palpites = 1
 print('\033[1;33m----Jogo de Adivinha----\033[m')
 while num == '':
     num = randint(1,10)
@@ -10,7 +10,7 @@ while num == '':
     if num == palpite:
         print('Parabéns você \033[1;32mACERTOU!\033[m \nNossa.. e de primeira, Sortudo!!')
     while num != palpite:
-        palpite = int(input('Você \033[1;31m[ERROU]!\033[m Tente novamente: '))
+        palpite = int(input('Você \033[1;31m[ERROU]\033[m Tente novamente: '))
         palpites += 1
         if num == palpite:
-            print(f'Parabens você \033[1;32mACERTOU!\033[m Com {palpites} palpites.')
+            print(f'Parabens você \033[1;32mACERTOU!\033[m Com \033[1:32m{palpites}\033[m palpites.')
