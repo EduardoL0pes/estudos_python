de756d09895e09e99c6dd9d1df0681b95f847710
Eduardo <eduardo_lopes010@hotmail.com>
2025-08-29 15:57:14 -0300

    correções

--- Aula10/desafio05.py
+++ Aula10/desafio05.py
@@ -1,21 +1,29 @@
-#Definindo os parâmetros de PA
-termo = int(input('Primeiro termo: '))
-razao = int(input('Razão: '))
-qt_termos = 10
-#Inicializando variáveis
-contador = 0
-termo_atual = termo
-#Gerando PA
-print('Progressão Aritmética')
-while contador < qt_termos:
-    print(termo_atual, end=' > ')
-    termo_atual += razao #Calcula o próximo termo
-    contador += 1  #Incrementa o contador
+#Opção1
+# # Definindo os parâmetros de PA
+# termo = int(input('Primeiro termo: '))
+# razao = int(input('Razão: '))
+# qt_termos = 10
+# # Inicializando variáveis
+# contador = 0
+# termo_atual = termo
+# # Gerando PA
+# print('Progressão Aritmética')
+# while contador < qt_termos:
+#     print(termo_atual, end=' > ')
+#     termo_atual += razao  # Calcula o próximo termo
+#     contador += 1  # Incrementa o contador
+# print('FIM')
 
-res = input('Deseja continar a progressão? [S/N] ').upper
-if res == 'S':
-    nv_termos = 20
-    while termo_atual < nv_termos:
-        print(termo_atual, end=' > ')
-        termo_atual += razao
-        contador += 1
+#Opção2
+print('Gerador de PA')
+print('-='*10)
+termo = int(input('Primeiro Termo: '))
+razao = int(input('Razão do PA: '))
+qt_termos = termo
+cont = 1
+
+while cont <= 10:
+    print(f'{termo} > ', end=' ')
+    termo += razao
+    cont += 1
+print('FIM')
