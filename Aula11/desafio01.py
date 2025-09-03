a549c7b295e2c71cb1b1529ed26d5e2f50efd256
Eduardo <eduardo_lopes010@hotmail.com>
2025-09-03 19:38:04 -0300

    novos exercicios

---
+++ Aula11/desafio01.py
@@ -0,0 +1,8 @@
+num = quant = soma = 0
+while True:
+    num = int(input('Digite um valor: '))
+    if num == 999:
+        break
+    quant += 1
+    soma += num
+print(f'Você digitou {quant} números e a soma é {soma}')
