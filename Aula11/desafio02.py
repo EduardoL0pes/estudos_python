08a7f33a53bedd48dd7c31a42ca7f87c141268b6
Eduardo <eduardo_lopes010@hotmail.com>
2025-09-04 12:24:29 -0300

    novos desafios

--- Aula11/desafio02.py
+++ Aula11/desafio02.py
@@ -1,7 +1,25 @@
-tab = int(input('Digite um número para ver a sua TABUADA: '))
-num = 0
+# tab = int(input('Digite um número para ver a sua TABUADA: '))
+# num = 0
+# while True:
+#     num += 1
+#     if num == 11:
+#          break
+#     print(f'{tab}x{num} = {num*tab}')
+
+
+# tab = int(input('Digite um número para mostrar sua TABUADA: '))
+# num = 0
+# while num < 10:
+#     num += 1
+#     print(f'{tab}x{num} = {num * tab}')
+
+
 while True:
-    num += 1
-    if num == 11:
+    tab = int(input('Quer ver a TABUADA de qual valor? '))
+    print('-' * 30)
+    if tab < 0:
         break
-    print(f'{tab}x{num} = {num*tab}')
+    for c in range(1, 11):
+        print(f'{tab} x {c} = {tab*c}')
+    print('-' * 30)
+print('Finalizando o Programa...')
