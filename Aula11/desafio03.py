950349e0e922df87e4378b7de1bb15f898a7a0de
Eduardo <eduardo_lopes010@hotmail.com>
2025-09-05 14:17:29 -0300

    novos desafios

--- Aula11/desafio03.py
+++ Aula11/desafio03.py
@@ -5,17 +5,22 @@ vit = 0
 while True:
     print('\033[1;33m-=\033[m'*20)
     play = int(input('Digite um número: '))
-    esc = input('Deseja escolher [P/I]? ').upper().strip()
+    esc = input('Par ou Ìmpar [P/I]? ').upper().strip()
     comp = randint(1, 10)
     s = play + comp
     print('\033[1;33m-=\033[m'*20)
     if s % 2 == 0 and esc == 'P':
-        print('\033[1;32mVOCÊ GANHOU!\033[m')
+        print('\033[1;32mVOCÊ VENCEU!\033[m')
         vit += 1
+    elif s % 2 != 0 and esc == 'I':
+        print('\033[1;32mVOCÊ VENCEU!\033[m')
+        vit += 1
+    elif s % 2 == 0 and esc != 'P':
+        print('\033[1;31mVOCÊ PERDEU!\033[m')
+        print(f'Você jogou {play} e o computador {comp}. Total de {s} deu PAR')
+        break
     else:
         print('\033[1;31mVOCÊ PERDEU!\033[m')
+        print(f'Você jogou {play} e o computador {comp}. Total de {s} deu ÌMPAR')
         break
-print(f'FIM DE JOGO!, VOCÊ GANHOU \033[1;32m{vit}\033[m VEZES.')
-
-print(f'computador {comp}')
-print(f'Player {play}')
+print(f'FIM DE JOGO!, VOCÊ VENCEU \033[1;32m{vit}\033[m VEZES.')
