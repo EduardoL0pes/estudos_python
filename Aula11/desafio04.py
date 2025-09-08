df4b24ddb9a76ddc74ca4000ffabf32fbf827577
Eduardo <eduardo_lopes010@hotmail.com>
2025-09-08 14:34:23 -0300

    novos desafios

--- Aula11/desafio04.py
+++ Aula11/desafio04.py
@@ -1,20 +1,27 @@
 maiores = 0
 masc = 0
 fem = 0
+sexo = ''
 while True:
-    idade = int(input('Digite a idade: '))
-    sexo = input('Digite o sexo: [M/F] ').upper().strip()
-    conf = input('Deseja continuar o cadastro? [S/N] ').upper().strip()
-    if conf == 'S':
-        if idade >= 18:
-            maiores += 1
-        if sexo == 'M':
-            masc += 1
-        if sexo == 'F' and idade < 20:
-            fem += 1
-    else:
-        print('----FIM DO PROGRAMA----')
-        print(f'Um total de {maiores} pessoas foram cadastradas com mais de 18 anos.')
-        print(f'Foram cadastrados {masc} homens.')
-        print(f'Foram {fem} mulheres cadastradas com menos de 20 anos.')
-        break
+    #idade = int(input('Digite a idade: '))
+    while sexo != 'M' or sexo != 'F':
+        sexo = input('Digite o sexo: [M/F] ').upper().strip()
+        if sexo == 'M' or sexo == 'F':
+            idade = int(input('Digite a idade: '))
+            conf = input('Deseja continuar o cadastro? [S/N] ').upper().strip()
+            if conf == 'S':
+                if idade >= 18:
+                    maiores += 1
+                if sexo == 'M':
+                    masc += 1
+                if sexo == 'F' and idade < 20:
+                    fem += 1
+            else:
+                print('-='*30)
+                print('CADASTRO REALIZADADO COM SUCESSO !')
+                print('----FIM DO PROGRAMA----')
+                print(f'Um total de {maiores} pessoas foram cadastradas com mais de 18 anos.')
+                print(f'Foram cadastrados {masc} homens.')
+                print(f'Foram {fem} mulheres cadastradas com menos de 20 anos.')
+                break
+    break
