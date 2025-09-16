8a0e398578c19f2b0549b2bdfa33935ca0affa28
Eduardo <eduardo_lopes010@hotmail.com>
2025-09-16 15:22:55 -0300

    exercicios

--- Aula11/desafio05.py
+++ Aula11/desafio05.py
@@ -1,20 +1,26 @@
-total_gasto = mais1000 = menor_valor = 0
-menorn = ''
+total_gasto = mais1000 = menor_valor = cont = 0
+menorn = ' '
 print('====Loja do Povão====')
 while True:
     produto = input('Nome do produto: ')
-    preco = int(input('Preço: R$'))
+    preco = float(input('Preço: R$'))
+    cont += 1
+    total_gasto += preco
+    if preco > 1000:
+        mais1000 += 1
+    if cont == 1 or preco < menor_valor: #Simplificada
+        menor_valor = preco
+        menorn = produto
+    # else:
+    #     if preco < menor_valor:
+    #         menor_valor = preco  #Detalhada
+    #         menorn = produto
     conf = ' '
     while conf not in 'SN':
         conf = input('Deseja continuar? [S/N]').upper().strip()[0]
-    total_gasto += preco
-    menor_valor += preco
-    if menor_valor < preco:
-        menorn = produto
-    if preco > 1000:
-        mais1000 += 1
     if conf == 'N':
         break
-print(f'Quantidade de produtos que custam mais de R$1000: {mais1000}')
-print(f'O nome do produto mais barato: {produto}')
+print('{:-^40}'. format('Fim do programa'))
+print(f'Quantidade de produtos que custam mais de R$1000.00: {mais1000}')
+print(f'O nome do produto mais barato foi {menorn} que custa R${menor_valor:.2f}')
 print(f'Total gasto: R${total_gasto:.2f}')
