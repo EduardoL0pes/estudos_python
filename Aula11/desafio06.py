5d478ea5c2ef8a286d4a6358d25d21e5dccf4bcc
Eduardo <eduardo_lopes010@hotmail.com>
2025-09-17 12:15:26 -0300

    exercicios

--- Aula11/desafio06.py
+++ Aula11/desafio06.py
@@ -1,10 +1,20 @@
-c = 50
-v = 20
-d = 10
-u = 1
+valor = int(input('Qual valor você quer sacar? R$'))
+total = valor
+ced = 50
+totced = 0
 while True:
-    valor = int(input('Qual o valor a ser sacado? '))
-    if valor > 50:
-        valor /= c
-    break
-print(valor)
+    if total >= ced:
+        total -= ced
+        totced += 1
+    else:
+        if totced > 0:
+            print(f'São {totced} cédulas de R${ced}')
+        if ced == 50:
+            ced = 20
+        elif ced == 20:
+            ced = 10
+        elif ced == 10:
+            ced = 1
+        totced = 0
+        if total == 0:
+            break
