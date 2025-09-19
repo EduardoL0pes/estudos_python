8f5d547a4ad3aa221c40c214a9ecf6760aa40695
Eduardo <eduardo_lopes010@hotmail.com>
2025-09-19 15:12:06 -0300

    Jogo adivinhação 2.0

---
+++ Aula11/adivinhacao2.py
@@ -0,0 +1,43 @@
+from random import randint
+
+print('{:-^31}'.format(' Jogo de Adivinhação 2.0 '))
+
+while True:
+    print('DIFICULDADES:\n'
+          '1 - Fácil\n'
+          '2 - Médio\n'
+          '3 - Difícil')
+    dif = int(input('Escolha a Dificuldade: '))
+    if dif == 1:
+        limite = 10
+        tentativa = 5
+        print('DIFICULDADE: Fácil\n Acerte o número correto entre 1 e 10 com 5 tentativas. Boa Sorte!')
+    elif dif == 2:
+        limite = 50
+        tentativa = 7
+        print('DIFICULDADE: Médio\n Acerte o número correto entre 1 e 50 com 7 tentativas. Boa Sorte!')
+    elif dif == 3:
+        limite = 100
+        tentativa = 7
+        print('DIFICULDADE: Difícil\n Acerte o número correto entre 1 e 100 com 10 tentativas. Boa Sorte!')
+    else:
+        print('Opção inválida. Tente novamente')
+        continue
+    computador = randint(1, limite)
+    jogador = int(input('Digite seu palpite: '))
+    while jogador != computador > 0:
+        tentativa -= 1
+        if jogador > computador:
+            print('MENOS...')
+        else:
+            print('MAIS...')
+        if tentativa > 0:
+            print(f'Você ainda tem {tentativa} tentativas restantes.')
+            print('-='*30)
+            print()
+            jogador = int(input('Digite seu palpite: '))
+        else:
+            print('GAME OVER! O número era', computador)
+    if jogador == computador:
+        print('PARABÉNS, VOCÊ VENCEU!!!')
+    break
