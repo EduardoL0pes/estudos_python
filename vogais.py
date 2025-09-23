30762f9aea4e84b1f8a9d47d13cb90a9cd9e37f3
Eduardo <eduardo_lopes010@hotmail.com>
2025-09-23 11:01:06 -0300

    Contador de vogais

--- vogais.py
+++ vogais.py
@@ -1,5 +1,25 @@
-nome = input('Digite uma palavra: ').strip().upper()
-contador = 0
-while nome in 'AEIOU':
-    contador += 1
-print(f'A palavra {nome} tem {contador} vogais.')
+palavra = input('Digite uma palavra/frase: ').strip().lower()
+remove_esp = palavra.replace(' ','')
+
+i = 0
+total_vogais = 0
+total_cons = 0
+
+while i < len(remove_esp):
+    letra = remove_esp[i]
+    if letra in 'aeiou':
+        total_vogais += 1
+    elif letra.isalpha():
+        total_cons += 1
+    i += 1
+print(f'nome: {palavra} tem {total_vogais} vogais e {total_cons} consoantes.')
+
+# #Usando loop for
+# palavra = 'programador'
+# vogais = 0
+# for i in range(len(palavra)):
+#     letra = palavra[i].lower()
+#     if letra in 'aeiou':
+#         vogais += 1
+#     i += 1
+# print(f'nome: {palavra} tem {vogais} vogais.')
