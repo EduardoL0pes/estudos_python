3e1f43867b90bc4dfa297e909de8da91bd013163
Eduardo <eduardo_lopes010@hotmail.com>
2025-09-24 14:56:36 -0300

    tuplas, desafio 02

---
+++ Aula12/desafio02.py
@@ -0,0 +1,5 @@
+times = 'flamengo', 'cruzeiro', 'palmeiras', 'mirassol', 'botafogo', 'bahia', 'são paulo', 'fluminense', 'bragantino', 'corinthians', 'ceará', 'grêmio', 'internacional', 'santos', 'atlético-MG', 'vasco', 'vitória', 'juventude', 'fortaleza', 'sport'
+print(f'Os 5 primeiros colocados: {times[:5]}')
+print(f'Os 4 últimos: {times[16:]}')
+print(f'Ordem alfabética: {sorted(times)}')
+print(f'Em {times.index('fluminense')+1}ª posição')
