e3379b55ec89c18eda9c366fef75e0c338877c43
Eduardo <eduardo_lopes010@hotmail.com>
2025-09-26 17:13:52 -0300

    tuplas, desafio 05

--- Aula12/desafio05.py
+++ Aula12/desafio05.py
@@ -1,2 +1,7 @@
 listagem = 'Pão', 1, 'Leite', 3.10, 'Mortadela', 3.50, 'Mussarela', 4.50
-print(listagem)
+print('-'*30)
+print('LISTAGEM DE PREÇO'.center(30))
+print('-'*30)
+print('{:.<20}R$ {:.2f}'.format(listagem[0], listagem[1]))
+print('{:.<20}R$ {:.2f}'.format(listagem[2], listagem[3]))
+print('{:.<20}R$ {:.2f}'.format(listagem[4], listagem[5]))
