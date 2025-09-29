64df8316aae632a719a8736fb871415ae62980ab
Eduardo <eduardo_lopes010@hotmail.com>
2025-09-29 17:34:43 -0300

    desafio01

--- Aula12/desafio01.py
+++ Aula12/desafio01.py
@@ -1,27 +1,32 @@
 #Opção01
-num_extenso = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco',
-               'seis', 'sete', 'oito', 'nove', 'dez',
-               'onze', 'doze', 'treze', 'quatorze', 'quinze',
-               'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
-num = 0
-for c in range(num, len(num_extenso)):
-    num = int(input('Digite um número entre 0 e 20: '))
-    if num > 20 or num < 0:
-        print('Número informado não existe, tente novamente.')
-    else:
-        print(f'O número que você digitou é {num_extenso[num]}')
-        break
+# num_extenso = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco',
+#                'seis', 'sete', 'oito', 'nove', 'dez',
+#                'onze', 'doze', 'treze', 'quatorze', 'quinze',
+#                'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
+# num = 0
+# for c in range(num, len(num_extenso)):
+#     num = int(input('Digite um número entre 0 e 20: '))
+#     if num > 20 or num < 0:
+#         print('Número informado não existe, tente novamente.')
+#     else:
+#         print(f'O número que você digitou é {num_extenso[num]}')
 
 #Opção02
-# cont = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco',
-#         'seis', 'sete', 'oito', 'nove', 'dez',
-#         'onze', 'doze', 'treze', 'quatorze', 'quinze',
-#         'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte' )
-#
-# while True:
-#     num = int(input('Digite um número entre 0 e 20: '))
-#     if 0 <= num <= 20:
-#         print(f'Você digitou o número {cont[num]}')
-#         break
-#     else:
-#         print('Tente novamente.', end='')
+cont = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco',
+        'seis', 'sete', 'oito', 'nove', 'dez',
+        'onze', 'doze', 'treze', 'quatorze', 'quinze',
+        'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte' )
+
+while True:
+    num = int(input('Digite um número entre 0 e 20: '))
+    if 0 <= num <= 20:
+        print(f'Você digitou o número {cont[num]}')
+        conf = ' '
+        while conf not in 'SN':
+            conf = input('Deseja continuar? [S/N] ').upper().strip()[0]
+            print('-' * 20)
+        if conf == 'N':
+            print('Fim do Programa!')
+            break
+    else:
+        print('Tente novamente.', end='')
