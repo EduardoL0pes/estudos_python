0831c02430deaf7e57ad284f007da12bedaac2c5
Eduardo <eduardo_lopes010@hotmail.com>
2025-09-29 17:23:11 -0300

    desafio06

--- Aula12/desafio01.py
+++ Aula12/desafio01.py
@@ -1,4 +1,8 @@
-num_extenso = 'zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte'
+#Opção01
+num_extenso = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco',
+               'seis', 'sete', 'oito', 'nove', 'dez',
+               'onze', 'doze', 'treze', 'quatorze', 'quinze',
+               'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
 num = 0
 for c in range(num, len(num_extenso)):
     num = int(input('Digite um número entre 0 e 20: '))
@@ -7,3 +11,17 @@ for c in range(num, len(num_extenso)):
     else:
         print(f'O número que você digitou é {num_extenso[num]}')
         break
+
+#Opção02
+# cont = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco',
+#         'seis', 'sete', 'oito', 'nove', 'dez',
+#         'onze', 'doze', 'treze', 'quatorze', 'quinze',
+#         'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte' )
+#
+# while True:
+#     num = int(input('Digite um número entre 0 e 20: '))
+#     if 0 <= num <= 20:
+#         print(f'Você digitou o número {cont[num]}')
+#         break
+#     else:
+#         print('Tente novamente.', end='')
---
+++ Aula12/desafio06.py
@@ -0,0 +1,3 @@
+palavra = 'Aprender', 'Estudar', 'Escutar'
+for c in range(0, len(palavra)):
+    print(f'{palavra[c]}  {palavra[0]}')
