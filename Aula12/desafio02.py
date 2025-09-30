752b70e1a43311ade190ac1a46918a56024e755d
Eduardo <eduardo_lopes010@hotmail.com>
2025-09-30 13:26:12 -0300

    desafio01

--- Aula12/desafio02.py
+++ Aula12/desafio02.py
@@ -1,5 +1,8 @@
-times = 'flamengo', 'cruzeiro', 'palmeiras', 'mirassol', 'botafogo', 'bahia', 'são paulo', 'fluminense', 'bragantino', 'corinthians', 'ceará', 'grêmio', 'internacional', 'santos', 'atlético-MG', 'vasco', 'vitória', 'juventude', 'fortaleza', 'sport'
+times = ('flamengo', 'cruzeiro', 'palmeiras', 'mirassol', 'botafogo',
+         'bahia', 'são paulo', 'fluminense', 'bragantino', 'corinthians',
+         'ceará', 'grêmio', 'internacional', 'santos', 'atlético-MG',
+         'vasco', 'vitória', 'juventude', 'fortaleza', 'sport')
 print(f'Os 5 primeiros colocados: {times[:5]}')
-print(f'Os 4 últimos: {times[16:]}')
+print(f'Os 4 últimos: {times[-4:]}')
 print(f'Ordem alfabética: {sorted(times)}')
-print(f'Em {times.index('fluminense')+1}ª posição')
+print(f'O time do {times[7]} esta em {times.index('fluminense')+1}ª posição')
