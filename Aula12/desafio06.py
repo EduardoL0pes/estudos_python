b33086c575ed13dccfac956c11d3d3beed233d54
Eduardo <eduardo_lopes010@hotmail.com>
2025-09-30 15:33:44 -0300

    desafio06

--- Aula12/desafio06.py
+++ Aula12/desafio06.py
@@ -1,3 +1,6 @@
-palavra = 'Aprender', 'Estudar', 'Escutar'
-for c in range(0, len(palavra)):
-    print(f'{palavra[c]}  {palavra[0]}')
+palavras = 'aprender', 'estudar', 'escutar', 'linguagem', 'python', 'curso'
+for p in palavras:  ##Percorre cada palavra na tupla
+    print(f'\nNa palavra {p.upper()} temos ', end='')
+    for letra in p: ##Percorre cada letra na tupla
+        if letra.lower() in 'aeiou':
+            print(f'{letra}', end=' ')
