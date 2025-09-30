24c8ce65c97c529ae23a39f81a8effb76f7ca767
Eduardo <eduardo_lopes010@hotmail.com>
2025-09-30 13:41:09 -0300

    desafio03

--- Aula12/desafio03.py
+++ Aula12/desafio03.py
@@ -1,6 +1,18 @@
-import random
+#Opção01
+# import random
+#
+# num_tup = tuple(random.randint(0,10) for c in range(5))
+# print(f'{num_tup} \n', end='')
+# print(f'O maior número sorteado: {max(num_tup)}')
+# print(f'O menor número sorteado: {min(num_tup)}')
 
-num_tup = tuple(random.randint(0,10) for c in range(5))
-print(f'{num_tup} \n', end='')
-print(f'O maior número sorteado: {max(num_tup)}')
-print(f'O menor número sorteado: {min(num_tup)}')
+
+#Opção02
+from random import randint
+
+numero = (randint(1,10), randint(1,10), randint(1,10), randint(1,10), randint(1,10))
+print('Os valores sorteados são: ', end='')
+for n in numero:
+    print(f'{n}', end=' ')
+print(f'\nO maior número sorteado: {max(numero)}')
+print(f'O menor número sorteado: {min(numero)}')
