a23c7cc1728dabd675da36335d1a770cbdf1299f
Eduardo <eduardo_lopes010@hotmail.com>
2025-10-07 14:44:10 -0300

    desafio03

--- Aula13/desafio03.py
+++ Aula13/desafio03.py
@@ -1,5 +1,15 @@
 valores = []
-for c in range(0, 3):
+for c in range(0, 5):
     num = int(input('Digite um valor: '))
+    valores.append(num)
 
-print(valores)
+for i in range(len(valores)):
+    menor_indice = i
+
+    for v in range(i + 1, len(valores)):
+        if valores[v] < valores[menor_indice]:
+            menor_indice = v
+
+    valores[i], valores[menor_indice] = valores[menor_indice], valores[i]
+
+print('Lista Ordenada: ', valores)
