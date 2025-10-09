4d1baccd77dc65c06d905683cee363afd0cddabd
Eduardo <eduardo_lopes010@hotmail.com>
2025-10-09 14:18:03 -0300

    desafio06

--- Aula13/desafio06.py
+++ Aula13/desafio06.py
@@ -1,11 +1,39 @@
-valores = []
-ex = input('Digite uma expressão: ').strip()
-valores.append(ex)
+## Versão simples
 
-for c in range(len(valores)):
-    analise = valores[c]
-    if '()' in analise:
+# possui_parentes = 0
+# ex = input('Digite uma expressão: ').strip()
+#
+# for c in ex:
+#     if c == '(':
+#         possui_parentes += 1
+#     elif c == ')':
+#         possui_parentes -= 1
+#         if possui_parentes < 0:
+#             break
+#
+# if possui_parentes == 0:
+#     print('Expressão está correta!')
+# else:
+#     print('Expressão está incorreta.')
+
+##Versão com pilha
+
+ex = input('Digite uma expressão: ').strip()
+
+pilha = []
+
+for c in ex:
+    if c == '(':
+        pilha.append('(')
+    elif c == ')':
+        if len(pilha) == 0:
+            print('Expressão está incorreta.')
+            break
+        else:
+            pilha.pop()
+else:
+    if len(pilha) == 0:
         print('Expressão está correta!')
     else:
-        print('Expressão inválida.')
-
+        print('Expressão está incorreta!')
+#((a+b)*1)
