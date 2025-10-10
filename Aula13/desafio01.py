45d4347041935d916ac21cd920b6709c48e849f0
Eduardo <eduardo_lopes010@hotmail.com>
2025-10-10 10:54:04 -0300

    desafio01

--- Aula13/desafio01.py
+++ Aula13/desafio01.py
@@ -17,3 +17,29 @@ for i, v in enumerate(valores):
     if v == menor:
         posicao_menor.append(i)
 print(f'O menor valor digitado foi {menor} nas posições {posicao_menor}')
+
+# valor_num = []
+# maior = menor = 0
+# for c in range(0,5):
+#     valor_num.append(int(input(f'Digite um valor para posição {c}: ')))
+#
+#     if c == 0:
+#         maior = menor = valor_num[c]
+#     else:
+#         if valor_num[c] > maior:
+#             maior = valor_num[c]
+#         if valor_num[c] < menor:
+#             menor = valor_num[c]
+#
+# print('-='*30)
+# print(f'Você digitou os valores {valor_num}')
+# print(f'O maior valor digitado foi {maior} nas posições ', end='')
+# for i, v in enumerate(valor_num):
+#     if v == maior:
+#         print(f'{i}..', end='')
+# print()
+# print(f'O menor valor digitado foi {menor} nas posições ', end='')
+# for i, v in enumerate(valor_num):
+#     if v == menor:
+#         print(f'{i}..', end='')
+# print()
