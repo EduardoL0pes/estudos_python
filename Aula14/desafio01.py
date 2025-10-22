ba2251fad574e6e59183059bb7f3ef532a8e632f
Eduardo <eduardo_lopes010@hotmail.com>
2025-10-22 19:47:41 -0300

    desafio01

--- Aula14/desafio01.py
+++ Aula14/desafio01.py
@@ -36,4 +36,38 @@
 # print(f'O menor peso foi de {min_peso}Kg. Peso de {nomes_min}.')
 
 
-#Opção02
+# Opção02
+princ = []
+temp = []
+mai = men = 0
+while True:
+    temp.append(input('Digite seu nome: ').strip().title())
+    temp.append(float(input('Peso: ')))
+
+    if len(princ) == 0:
+        mai = men = temp[1]
+    else:
+        if temp[1] > mai:
+            mai = temp[1]
+        if temp[1] < men:
+            men = temp[1]
+
+    princ.append(temp[:])
+    temp.clear()
+
+    resp = input('Deseja continuar? [S/N] ').strip().upper()[0]
+    if resp in 'N':
+        break
+
+print(f'Ao todo, você cadastrou {len(princ)} pessoas.')
+
+print(f'O maior peso foi de {mai}Kg. Peso de ', end='')
+for p in princ:
+    if p[1] == mai:
+        print(f'[{p[0]}] ', end='')
+print()
+
+print(f'O menor peso foi de {men}Kg. Peso de ', end='')
+for p in princ:
+    if p[1] == men:
+        print(f'[{p[0]}] ', end='')
