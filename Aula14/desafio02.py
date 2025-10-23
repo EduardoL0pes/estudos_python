3c230a5786bbaeeec9e2b4ae25f52dd6f0003cb8
Eduardo <eduardo_lopes010@hotmail.com>
2025-10-23 14:26:56 -0300

    desafio02

--- Aula14/desafio02.py
+++ Aula14/desafio02.py
@@ -1,20 +1,23 @@
-numeros = []
-for n in range(0, 7):
-    numeros.append(int(input(f'Digite {n+1}° valor: ')))
-print(f'Lista completa: {numeros}')
+# numeros = []
+# for n in range(0, 7):
+#     numeros.append(int(input(f'Digite {n+1}° valor: ')))
+# print(f'Lista completa: {numeros}')
+#
+# num_ordem_p = []
+# for num in numeros:
+#     if num % 2 == 0:
+#         num_ordem_p.append(num)
+#
+# num_ordem_p.sort()
+# print(f'Números pares: {num_ordem_p}')
+#
+# num_ordem_i = []
+# for num in numeros:
+#     if num % 2 == 1:
+#         num_ordem_i.append(num)
+#
+# num_ordem_i.sort()
+# print(f'Números ímpares: {num_ordem_i}')
 
-num_ordem_p = []
-for num in numeros:
-    if num % 2 == 0:
-        num_ordem_p.append(num)
 
-num_ordem_p.sort()
-print(f'Números pares: {num_ordem_p}')
-
-num_ordem_i = []
-for num in numeros:
-    if num % 2 == 1:
-        num_ordem_i.append(num)
-
-num_ordem_i.sort()
-print(f'Números ímpares: {num_ordem_i}')
+# Opção02
