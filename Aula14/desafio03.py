d71c4c08e144c6e80f1fb44051a280cde78a22d5
Eduardo <eduardo_lopes010@hotmail.com>
2025-10-28 15:40:03 -0300

    desafio02

--- Aula14/desafio03.py
+++ Aula14/desafio03.py
@@ -1,13 +1,24 @@
-matriz = []
+# matriz = []
+#
+# for i in range(3):
+#     linha = []
+#     for j in range(3):
+#         valor = int(input(f'Digite um valor [{i}, {j}]: '))
+#         linha.append(valor)
+#     matriz.append(linha)
+#
+# for linha in matriz:
+#     print(*linha)        # * Desempacota a lista, mostrando sem os conchetes e as virgulas
+#     #print(linha)
 
-for i in range(3):
-    linha = []
-    for j in range(3):
-        valor = int(input(f'Digite um valor [{i}, {j}]: '))
-        linha.append(valor)
-    matriz.append(linha)
 
-for linha in matriz:
-    print(*linha)        # * Desempacota a lista, mostrando sem os conchetes e as virgulas
-    #print(linha)
-
+# Opção02
+matriz = [[0,0,0], [0,0,0], [0,0,0]]
+for l in range(0, 3):
+    for c in range(0, 3):
+        matriz[l][c] = int(input(f'Digite um valor {l, c}: '))
+print('-='*30)
+for l in range(0, 3):
+    for c in range(0, 3):
+        print(f'[{matriz[l][c]:^5}]', end='')
+    print()
