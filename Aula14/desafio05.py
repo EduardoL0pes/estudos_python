f73fe736074097d2dbbdcc65c679b9eb4f784ee2
Eduardo <eduardo_lopes010@hotmail.com>
2025-11-03 11:55:22 -0300

    desafio05

--- Aula14/desafio05.py
+++ Aula14/desafio05.py
@@ -23,3 +23,28 @@ for _ in range(quantidade):
 for i, j in enumerate(todos_jogos):
     sleep(0.5)
     print(f'jogo {i+1}: {j}')
+
+# Opção02
+# from random import randint
+#
+# lista = list()
+# jogos = list()
+# quant = int(input('Quantos jogos você quer que eu sorteie? '))
+# tot = 1
+#
+# while tot <= quant:
+#     cont = 0
+#     while True:
+#         num = randint(1,60)
+#         if num not in lista:
+#             lista.append(num)
+#             cont += 1
+#         if cont >= 6:
+#             break
+#     lista.sort()
+#     jogos.append(lista[:])
+#     lista.clear()
+#     tot += 1
+#
+# for i, l in enumerate(jogos):
+#     print(f'Jogo {i+1}: {l}')
