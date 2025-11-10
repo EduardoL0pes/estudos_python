41d681660cb8f6e7cfc7d1483ee90b7b2f7c9801
Eduardo <eduardo_lopes010@hotmail.com>
2025-11-10 16:32:40 -0300

    desafio04

---
+++ Aula15/desafio04.py
@@ -0,0 +1,24 @@
+jogador = {
+    'nome': input('Nome do jogador: ').title().strip()
+}
+
+gols = []
+tot = 0
+
+partidas = int(input(f'Quantas partidas {jogador['nome']} jogou? '))
+for p in range(partidas):
+    gols_partidas = int(input(f'Quantos gols na partida {p+1}? '))
+    tot += gols_partidas
+    gols.append(gols_partidas)
+
+    jogador['gols'] = gols[:]
+    jogador['total'] = tot
+print('-'*30)
+
+for keys, values in jogador.items():
+    print(f'O campo {keys} tem o valor de {values}.')
+print('-'*30)
+
+print(f'O jogador {jogador['nome']} jogou {partidas} partidas.')
+for k, v in enumerate(gols):
+    print(f'Na partida {k}, fez {v} gols.')
