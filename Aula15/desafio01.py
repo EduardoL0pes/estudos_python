486538411651f5a6e122edc7447fb3b821c23bc0
Eduardo <eduardo_lopes010@hotmail.com>
2025-11-11 20:12:39 -0300

    desafio01

--- Aula15/desafio01.py
+++ Aula15/desafio01.py
@@ -1,10 +1,12 @@
 dados = dict()
 dados['nome'] = input('Nome: ').title()
 dados['media'] = float(input(f'Média de {dados['nome']}: '))
-print()
+print('-'*30)
 for k, v in dados.items():
     print(f'{k} é igual a {v}')
 if dados['media'] < 5:
     print('Situação REPROVADO.')
+elif dados['media'] < 7:
+    print('RECUPERAÇÃO.')
 else:
-    print('Situação APROVADO.')
+    print('APROVADO')
