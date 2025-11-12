ec554ec988d2a1a0b57ad3c2a7e2e08e04d699e9
Eduardo <eduardo_lopes010@hotmail.com>
2025-11-12 15:01:55 -0300

    desafio05

--- Aula15/desafio05.py
+++ Aula15/desafio05.py
@@ -1,28 +1,37 @@
-dic = dict()
-lista = list()
+pessoas = {}
+pessoas_lista = []
+mulheres = []
 tot = 0
-soma_idade = 0
-media = 0
-mulheres = list()
 
 while True:
-    dic['nome'] = input('Nome: ').title()
-    dic['sexo'] = input('Sexo [M/F]: ').upper()
-    dic['idade'] = int(input('Idade: '))
-    tot += 1
-    soma_idade += dic['idade']
-    media = soma_idade / tot
+    pessoas['nome'] = input('Nome: ').title()
+    pessoas['idade'] = int(input('Idade: '))
+    sexo = input('Sexo [M/F]: ').upper()
+    if sexo in 'MF':
+        pessoas['sexo'] = sexo
+    if sexo in 'F':
+        mulheres.append(pessoas['nome'])
+    elif sexo not in 'MF':
+        while sexo not in 'MF':
+            print('ERRO, somente M ou F. Tente novamente...')
+            sexo = input('Sexo [M/F]: ').upper()
+    tot += pessoas['idade']
 
-    lista.append(dic.copy())
+    print('Dados cadastrados com SUCESSO!')
+    pessoas_lista.append(pessoas.copy())
 
-    if dic['sexo'] == 'F':
-        mulheres.append(dic['nome'])
-
-    resp = input('Deseja continuar? [S/N]: ').strip()
-    if resp in 'Nn':
+    resp = input('Deseja continuar? [S/N]: ').upper()
+    if resp not in 'SN':
+        while resp not in 'SN':
+            print('ERRO, somente S ou N.')
+            resp = input('Deseja continuar? [S/N]: ').upper()
+    if resp in 'N':
         break
+media = tot / len(pessoas_lista)
 
-print('-'*30)
-print(f'Foram cadastradas {tot} pessoas.')
-print(f'A média de idade do grupo é de {media} anos.')
-print(f'As mulheres cadastradas foram {mulheres}.')
+print(f'Ao todo foram cadastradas {len(pessoas_lista)} pessoas.')
+print(f'A média de idade das pessoas cadastradas é igual a {media}')
+print(f'As mulheres cadastradas foram ', end='')
+for m in mulheres:
+    print(m, end=' ')
+
