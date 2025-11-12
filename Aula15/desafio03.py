3bd06df706f969506ff5021a6b77f28db132ecaa
Eduardo <eduardo_lopes010@hotmail.com>
2025-11-12 08:35:42 -0300

    desafio03

--- Aula15/desafio03.py
+++ Aula15/desafio03.py
@@ -1,16 +1,11 @@
 from datetime import date
 
 dados = {
-    'nome': input('Nome:').title(),
+    'nome': input('Nome: ').title(),
     'idade': int(input('Ano de Nascimento: ')),
     'ctps': int(input('Carteira de Trabalho (0 não tem): '))
 }
 dados['idade'] = date.today().year - dados['idade']
-
-if dados['ctps'] == 0:
-    print('=' * 30)
-    for keys, valores in dados.items():
-        print(f'{keys} tem o valor {valores}')
 
 if dados['ctps'] != 0:
     dados['contratação'] = int(input('Ano de contratação: '))
@@ -18,5 +13,5 @@ if dados['ctps'] != 0:
     dados['aposentadoria'] = (35 - (date.today().year - dados['contratação'])) + dados['idade']
     print('=' * 30)
 
-    for k, v in dados.items():
-        print(f'{k} tem o valor {v}')
+for k, v in dados.items():
+    print(f'{k} tem o valor {v}')
