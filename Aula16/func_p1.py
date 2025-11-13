30b901949a559cddc4404d8e6680f88d863cb93c
Eduardo <eduardo_lopes010@hotmail.com>
2025-11-13 17:10:29 -0300

    funções

---
+++ Aula16/func_p1.py
@@ -0,0 +1,15 @@
+# def mostralinha(): #Def (definição de função)
+#     print('-'*25)
+#
+# mostralinha()
+# print('HELLO WORLD'.center(25))
+# mostralinha()
+
+def mensagem(msg):
+    print('-'*25)
+    print(msg.center(25))
+    print('-'*25)
+
+mensagem('APRENDENDO SOBRE FUNÇÕES')
+mensagem('LINGUAGEM PYTHON')
+mensagem('HELLO WORLD')
