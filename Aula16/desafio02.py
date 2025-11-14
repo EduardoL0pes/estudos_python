c8e0b82931f9c57c573fd9174130905b7dac2b8b
Eduardo <eduardo_lopes010@hotmail.com>
2025-11-14 12:17:12 -0300

    desafio02

---
+++ Aula16/desafio02.py
@@ -0,0 +1,9 @@
+def escreva(txt):
+    traco = '-' * len(txt)
+    print(traco)
+    print(txt)
+    print(traco)
+
+escreva('Olá Mundo')
+escreva('Curso em Vídeo')
+escreva('Tentando aprender a linguagem python')
