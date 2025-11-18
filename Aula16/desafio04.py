dc57ca6ddfaa6c831fbb4ac1b63bf2a50f6f9e1d
Eduardo <eduardo_lopes010@hotmail.com>
2025-11-18 16:37:54 -0300

    desafio04

--- Aula16/desafio04.py
+++ Aula16/desafio04.py
@@ -6,13 +6,16 @@ def maior(*num):
     print('Analisando os valores passados...')
 
     for v in num:
-        sleep(0.4)
         print(f'{v}', end=' ')
+        sleep(0.4)
     print(f'Foram informados {len(num)} valores ao todo.')
-    print(f'O maior valor informado foi {max(num)}')
+    if len(num) > 0:
+        print(f'O maior valor informado foi {max(num)}.')
+    else:
+        print('O maior valor informado foi 0.')
 
 maior(5,6,3,7,8,9)
 maior(4,7,0)
 maior(1,2)
 maior(6)
-maior(0)
+maior()
