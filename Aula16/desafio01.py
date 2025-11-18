7e1ba44f10c49df808bfbb8f2bc312b0921c9911
Eduardo <eduardo_lopes010@hotmail.com>
2025-11-18 16:10:07 -0300

    desafio01

--- Aula16/desafio01.py
+++ Aula16/desafio01.py
@@ -1,8 +1,18 @@
+def area(larg, comp):
+    a = larg * comp
+    print(f'A área de um terrono {larg}x{comp} é de {a}m².')
+
 print('Controle de Terrenos'.center(30))
 print('-'*30)
+l = float(input('Largura: '))
+c = float(input('Comprimento: '))
+area(l, c)
 
-def area(l, c):
-    tot = l * c
-    print(f'A área de um terreno {l}x{c} é de {tot}m².')
 
-area(float(input('Largura (m): ')),float(input('Comrpimento (m): ')))
+# def area(l, c):
+#     tot = l * c
+#     print(f'A área de um terreno {l}x{c} é de {tot}m².')
+#
+# print('Controle de Terrenos'.center(30))
+# print('-'*30)
+# area(float(input('Largura (m): ')),float(input('Comrpimento (m): ')))
