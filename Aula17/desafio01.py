2a483716ffdf0a31712a82e8befb4f1cbeccf905
Eduardo <eduardo_lopes010@hotmail.com>
2025-11-19 15:19:37 -0300

    desafio01

---
+++ Aula17/desafio01.py
@@ -0,0 +1,18 @@
+def voto(nasc):
+    from datetime import date
+    global ano
+    data = date.today().year
+    calc = data - ano
+
+    print(f'Você tem {calc} anos.')
+    if 18 <= calc <= 59:
+        return 'VOTO OBRIGATÓRIO!'
+    if calc >= 60:
+        return 'VOTO OPCIONAL'
+    if calc <= 17:
+        return 'NÃO PODE VOTAR'
+
+
+ano = int(input('Ano de nascimento: '))
+print(voto(ano))
+
