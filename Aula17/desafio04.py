331c3adc0a3e253002d5df4ba99212033987aa24
Eduardo <eduardo_lopes010@hotmail.com>
2025-11-24 16:00:17 -0300

    desafio04

--- Aula17/desafio04.py
+++ Aula17/desafio04.py
@@ -1,9 +1,16 @@
-def leiaint(n):
-
-    n = int(input('Digite um número: '))
-    
-
-    return n
+def leiaint(msg):
+    ok = False
+    valor = 0
+    while True:
+        n = input(msg)
+        if n.isnumeric():
+            valor = int(n)
+            ok = True
+        else:
+            print('\033[0;31mERRO! Digite um número inteiro válido.\033[m')
+        if ok:
+            break
+    return valor
 
 n = leiaint('Digite um número: ')
 print(f'Você acabou de digitar o número {n}')
