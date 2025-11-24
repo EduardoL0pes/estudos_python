b71755c02a2bbef107820f94dc1cfef37fe76bd2
Eduardo <eduardo_lopes010@hotmail.com>
2025-11-24 17:09:33 -0300

    desafio06

--- Aula17/desafio06.py
+++ Aula17/desafio06.py
@@ -1,12 +1,33 @@
-def pyhelp():
-    print('-'*30)
-    print('SISTEMA DE AJUDA PyHELP'.center(30))
-    print('-'*30)
+from time import sleep
 
-    while True:
-        help(input('Função ou Biblioteca > '))
+c = ['\033[m',         # 0 - sem cores
+    '\033[0;30;41m',   # 1 - vermelho
+    '\033[0;30;42m',   # 2 - verde
+    '\033[0;30;44m',   # 3 - azul
+    '\033[7;40m'       # 4 - branco
+]
 
-        if help == 'fim':
-            break
+def ajuda(com):
+    titulo(f'Acessando o manual do comando \'{com}\'',3)
+    sleep(0.5)
+    print(c[4], end='')
+    help(com)
+    print(c[0], end='')
 
-pyhelp()
+def titulo(msg, cor=0):
+    tam = len(msg) + 4
+    print(c[cor], end='')
+    print('-' * tam)
+    print(f'  {msg}')
+    print('-' * tam)
+    print(c[0], end='')
+
+comando = ''
+while True:
+    titulo('SISTEMA DE AJUDA PyHELP',2)
+    comando = input('Função ou Biblioteca > ')
+    if comando.upper() == 'FIM':
+        break
+    else:
+        ajuda(comando)
+titulo('ATÉ LOGO',1)
